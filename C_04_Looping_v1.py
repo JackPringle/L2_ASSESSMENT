2bf826101ee9543b0df56b63ed5469a48c80cf33
JackPringle <101750172+JackPringle@users.noreply.github.com>
2024-05-08 13:06:32 +1200

    Commented now

---
+++ C_04_Looping_v1.py
@@ -0,0 +1,63 @@
+# Functions...
+
+# String checker function
+def string_checker(question, valid_list, error):
+    while True:
+
+        # Ask user for string (and put string in lowercase)
+        response = input(question).lower()
+
+        # Check the string is in the valid list
+        for var_item in valid_list:
+
+            # Check if the response matches the entire item or just the first character
+            # of the item (if it's not empty)
+            if response == var_item or (len(var_item) > 0 and response == var_item[0]):
+                return var_item
+
+        # If not in valid list, print error
+        print(error)
+        print()
+
+
+# Main Routine...
+
+# Lists of valid responses
+yes_no_list = ["yes", "no"]
+
+# Set problem values
+problem_number = 0
+another_problem = "yes"
+
+# Set calculations values
+number_of_calculations = 0
+another_calculation = "yes"
+
+# Keep looping until user does not want another problem
+while another_problem == "yes":
+
+    problem_number += 1
+
+    # Keep looping if the user wants another calculation
+    while another_calculation == "yes":
+        another_calculation = string_checker("Would you like to calculate something else? (y/n): ", yes_no_list,
+                                             "Please "
+                                             "enter "
+                                             "either "
+                                             "yes / no")
+
+        # Ask user what calculation they want, and print working
+        if another_calculation == "yes":
+            print("** Calculation here **\n")
+            number_of_calculations += 1
+
+    # After they say 'no' to another calculation, ask if they want to solve another problem
+    another_problem = string_checker("Would you like to solve a different problem / line? (y/n): ", yes_no_list,
+                                     "Please enter either "
+                                     "yes / no")
+    another_calculation = "yes"
+
+# Output the tally of calculations and problems
+print()
+print(f"Total number of calculations = {number_of_calculations}")
+print(f"Total number of problems = {problem_number}")
