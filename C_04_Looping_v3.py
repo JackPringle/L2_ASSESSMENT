0d19f57a83535484aeb3d77e3053d2d4e46b86e4
JackPringle <101750172+JackPringle@users.noreply.github.com>
2024-05-17 14:59:07 +1200

    Still broken (nearly there)

--- C_04_Looping_v3.py
+++ C_04_Looping_v3.py
@@ -35,15 +35,62 @@ another_problem = "yes"
 TOTAL_calculations = 0
 another_calculation = "yes"
 
-to_solve = "all"
+to_solve = None
 
 # Keep looping until user does not want another problem
 while another_problem == "yes":
 
+    problem_number += 1
     number_of_calculations = 0
 
+    while another_calculation == "yes":
+
+        if number_of_calculations == 0:
+
+            initial_calculation_wanted = string_checker(
+                "What would you like to calculate? (d / m / g / e) or press <enter> for "
+                "ALL: ", dmgea_list,
+                "Please enter "
+                "either distance /"
+                " midpoint / "
+                "gradient / "
+                "equation / or <enter> for ALL.")
+
+            if initial_calculation_wanted == "":
+                to_solve = "all"
+
+            else:
+                next_calculation = initial_calculation_wanted
+
+            break
+
+    if to_solve != "all":
+
+        # Ask user what calculation they want, and print working
+        if another_calculation == "yes":
+            number_of_calculations += 1
+
+            next_calculation = string_checker("What would you like to calculate? (d / m / g / e): ", dmge_list,
+                                              "Please enter "
+                                              "either distance /"
+                                              " midpoint / "
+                                              "gradient / "
+                                              "equation.")
+
+            # Call calculator function here to output working
+            print(f"** {next_calculation} calculation here **\n")
+
+        else:
+
+            another_calculation = "no"
+
+        another_calculation = string_checker("Would you like to calculate something else? (y/n): ", yes_no_list,
+                                             "Please "
+                                             "enter "
+                                             "either "
+                                             "yes / no")
+
     if to_solve == "all":
-        problem_number += 1
 
         to_solve_list = ["Distance", "Midpoint", "Gradient", "Line equation"]
 
@@ -56,43 +103,6 @@ while another_problem == "yes":
             input("Press <enter> to continue: ")
             print()
             print(f"** {item} calculation here **\n")
-
-    else:
-
-        while another_calculation == "yes":
-
-            if number_of_calculations == 0:
-
-                initial_calculation_wanted = string_checker(
-                    "What would you like to calculate? (d / m / g / e) or press <enter> for"
-                    "ALL: ", dmgea_list,
-                    "Please enter "
-                    "either distance /"
-                    " midpoint / "
-                    "gradient / "
-                    "equation / or <enter> for ALL.")
-
-            else:
-
-                another_calculation = string_checker("Would you like to calculate something else? (y/n): ", yes_no_list,
-                                                     "Please "
-                                                     "enter "
-                                                     "either "
-                                                     "yes / no")
-
-            # Ask user what calculation they want, and print working
-            if another_calculation == "yes":
-                number_of_calculations += 1
-
-                next_calculation = string_checker("What would you like to calculate? (d / m / g / e): ", dmge_list,
-                                                  "Please enter "
-                                                  "either distance /"
-                                                  " midpoint / "
-                                                  "gradient / "
-                                                  "equation.")
-
-                # Call calculator function here to output working
-                print("** Calculation here **\n")
 
     # After they say 'no' to another calculation, ask if they want to solve another problem
     another_problem = string_checker("Would you like to solve a different problem / line? (y/n): ", yes_no_list,
