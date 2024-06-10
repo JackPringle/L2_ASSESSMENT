7db767408522a6ab662d2289b85e2c3e0ef84b5b
JackPringle <101750172+JackPringle@users.noreply.github.com>
2024-06-11 10:57:31 +1200

    Final version (hopefully) and now includes commenting

--- C_04_Looping_v5.py
+++ C_04_Looping_v5.py
@@ -48,6 +48,12 @@ def int_checker(question, high):
 
 # Main Routine...
 
+# Heading
+print("*" * 42)
+print("***** COORDINATE GEOMETRY CALCULATOR *****")
+print("*" * 42)
+print()
+
 # Lists of valid responses
 yes_no_list = ["yes", "no"]
 dmge_list = ["distance", "midpoint", "gradient", "equation"]
@@ -63,70 +69,89 @@ to_solve = None
 # Keep looping until user does not want another problem
 while another_problem == "yes":
 
+    # Add to tally of problem numbers
     problem_number += 1
+
+    # Reset the number of calculations for each problem
     number_of_calculations = 0
+
+    # Automatically want to calculate something
     another_calculation = "yes"
+
+    # Create lists
     to_solve_list = []
     already_calculated = []
 
-    if another_calculation == "yes":
+    # Start of programme
 
-        if number_of_calculations == 0:
+    # For the first calculation only
+    if number_of_calculations == 0:
 
-            initial_calculation_wanted = int_checker(
-                "What would you like to calculate? Distance [1], Midpoint [2], Gradient [3], "
-                "Equation [4], or ALL [5]: ", 5)
+        # For first calculation, ask what they want to calculate (includes all mode option)
+        initial_calculation_wanted = int_checker(
+            "What would you like to calculate? Distance [1], Midpoint [2], Gradient [3], "
+            "Equation [4], or ALL [5]: ", 5)
 
+        # Set to solve to 'all' if chosen
+        if initial_calculation_wanted == 5:
+            to_solve = "all"
+
+        # If they chose dmge, add what they want to both lists, for solving and remembering what's been calculated
+        else:
             already_calculated.append(initial_calculation_wanted)
+            to_solve_list.append(initial_calculation_wanted)
+            to_solve = "one by one"
 
-            if initial_calculation_wanted == 5:
-                to_solve = "all"
-
-            else:
-                to_solve_list.append(initial_calculation_wanted)
-                to_solve = "one by one"
-
+    # If they didn't request 'all' calculations for the problem
     if to_solve != "all":
 
-        # Ask user what calculation they want, and print working
+        # Loop for each calculation
         while another_calculation == "yes":
 
-            # Loop headings in order for each calculation to solve
+            # Print the heading and calculation for requested calculations
             for item in to_solve_list:
                 number_of_calculations += 1
 
                 print()
                 print(f"** {dmge_list[int(item) - 1]} calculation here **\n")
 
+            # After printing the last calculation wanted, ask if they'd like another one
             another_calculation = string_checker("Would you like to calculate something else? (y/n): ", yes_no_list,
                                                  "Please "
                                                  "enter "
                                                  "either "
                                                  "yes / no")
 
+            # If they don't, break out, they will then be asked if they want another line/problem
             if another_calculation == "no":
                 break
 
+            # Remove past calculations from the list
             to_solve_list = []
 
-            # Ask what they want to calculate next
+            # Ask what they want to calculate next (does not include all mode this time since it's not the first
+            # calculation)
             next_calculation = int_checker(
                 "What would you like to calculate? Distance [1], Midpoint [2], Gradient [3], "
                 "Equation [4]: ", 4)
 
+            # If they already calculated that, tell them, then go back to top of loop, so they can re-enter
             if next_calculation in already_calculated:
                 print()
                 print(f"You have already calculated the {dmge_list[int(next_calculation) - 1]} for this problem!\n")
 
+            # If it has not already been calculated, add it to the lists, then go back to top of loop
             else:
                 already_calculated.append(next_calculation)
                 to_solve_list.append(next_calculation)
 
+    # If they want 'all' calculations for the problem
     if to_solve == "all":
 
+        # Heading
         print("**** ALL MODE ****\n")
 
-        # Loop headings in order for each calculation to solve
+        # For all of dmge, break up the calculations by getting user to press <enter> to continue
         for item in dmge_list:
             number_of_calculations += 1
 
@@ -135,13 +160,16 @@ while another_problem == "yes":
             print(f"** {item} calculation here **\n")
 
     # After they say 'no' to another calculation, ask if they want to solve another problem
+    # If they choose yes, program will go back to the top of the main loop
+    # If they choose no, program will end the main loop and start printing summaries
     another_problem = string_checker("Would you like to solve a different problem / line? (y/n): ", yes_no_list,
                                      "Please enter either "
                                      "yes / no")
 
+    # Once the user is done calculating, add together the number of calculations completed
     TOTAL_calculations += number_of_calculations
 
-# Output the tally of calculations and problems
+# Output the tally of total calculations and problems
 print()
 print(f"Total number of calculations = {TOTAL_calculations}")
 print(f"Total number of problems = {problem_number}")
